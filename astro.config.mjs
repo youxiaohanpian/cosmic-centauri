ead235c3ae68b3bd97cf0696305034c34b81f16f
youxiaohanpian <331907019@qq.com>
2025-03-25 17:23:11 +0800

    改一下排序和文档规则

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,14 +8,14 @@ export default defineConfig({
 		starlight({
 			title: 'My Docs',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/youxiaohanpian',
 			},
 			sidebar: [
 				{
 					label: 'Guides',
 					autogenerate: { directory: 'guides' }  // 改成自动生成 ✅ 需要使用小写的 'guides'
 					// items: [
-					// 	// Each item here is one entry in the navigation menu.
+					// 	// 每个导航有一个入口 item here is one entry in the navigation menu.
 					// 	{ label: 'Example Guide', slug: 'guides/example' },
 					// 	{ label: 'Git 操作图解', link: '/guides/git-guide' },  
 					// ],
@@ -25,6 +25,10 @@ export default defineConfig({
 					autogenerate: { directory: 'reference' }, // 改成自动生成
 				},
 			],
+			// tableOfContents: {
+			// 	minHeadingLevel: 2,    // 从二级标题开始显示
+			// 	maxHeadingLevel: 3     // 到三级标题结束
+			// }
 		}),
 	],
 });
--- src/content/docs/reference/example.md
+++ src/content/docs/reference/example.md
@@ -1,5 +1,5 @@
 ---
-title: Example Reference
+title: 参考
 description: A reference page in my new Starlight docs site.
 ---
 
